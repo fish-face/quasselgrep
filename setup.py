8ff5f9af30bbe1732bfb21e27359fce3475a6389
Ben Rosser <rosser.bjr@gmail.com>
2016-06-04 19:15:49 -0400

    Add setup.py file, rearrange repository structure
    A setuptools-based setup.py file is now included in the repository,
    in order to support easier installation. ("python setup.py install",
    for example). To accomodate this the sources have been moved inside
    a "quasselgrep" directory; this will be installed as a Python package.
    
    I added a "main" function to the quasselgrep script (and renamed it
    to quasselgrep/quasselgrep.py) that's used as an entry point for
    setuptools to auto-generate a "quasselgrep" script that will be installed
    into the system-wide scripts directory.
    
    Because current PyPI cannot parse Markdown long_descriptions, and because
    the de facto standard long_description is just the contents of the README
    file read into setuptools, I modified the standard setup.py script
    slightly by making it first attempt to use pypandoc to convert the README
    from MD to rST. If this fails due to an ImportError, the markdown
    README is used unmodified as the long_description; this means that
    pypandoc is not a dependency for end-users, but if you want to upload
    this package to PyPI, if you have pypandoc installed on your own system
    you can use it to ensure the description is nicely formatted.
    
    quasselgrep appears to still run after these changes, but I haven't
    gotten around to setting it up myself so that should be verified.
    
    (This is slightly biased to how I myself set up Python projects, but
    I based the setup.py file on this one:
    https://pypi.python.org/pypi/python-project-template/0.1.4.).

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+# Load the README as lonng_description.
+# Use pypandoc (if available) to convert MD to RST.
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert("README.md", "rst")
+
+except ImportError:
+
+    with open(os.path.join(here, 'README.md')) as f:
+        long_description = f.read()
+
+# Dependencies for the package, once installed.
+REQUIREMENTS = [
+    'python-dateutil',
+    'pycrypto',
+]
+
+setup(
+    name='quasselgrep',
+
+    version='0.1',
+
+    description='quasselgrep',
+
+    long_description=long_description,
+
+    author='Chris Le Sueur',
+
+    author_email='',
+
+    url='https://github.com/fish-face/quasselgrep',
+
+    license="GPLv2",
+
+    install_requires=REQUIREMENTS,
+
+    keywords=['quassel', 'quasselgrep', 'irc', 'logs'],
+
+    packages=find_packages(),
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for others
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    ],
+
+    # Entry points; setuptools will generate scripts that invoke the listed functions.
+    entry_points={
+        'console_scripts': ['quasselgrep = quasselgrep.quasselgrep:main']
+    },
+)
